3f85809f137c9f7080fe7836bc3825b0523d0608
Damar Wahyu Putra <165622543+DamarWahyu111@users.noreply.github.com>
2026-02-14 13:18:44 +0700

    Build static site

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'docs',
   typescript: {
     ignoreBuildErrors: true,
   }
 }
 
 export default nextConfig
+
