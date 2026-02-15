ca521db7606ed4d8b20a57eaac1093ea9bd62ce6
Damar Wahyu Putra <165622543+DamarWahyu111@users.noreply.github.com>
2026-02-15 10:39:56 +0700

    Fix next config syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // HAPUS ini kalau ada:
-  // output: 'export',
-  // basePath: '/fall-in-love',
-  
   images: {
     unoptimized: true
   }
 }
 
-module.exports = nextConfig
+export default nextConfig 
